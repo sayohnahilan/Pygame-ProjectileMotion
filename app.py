25df94b5539c8ce166c853c704987f1c4917715b
sayohnahilan <sayohnahilan@gmail.com>
2020-05-05 13:29:19 -0400

    stopping

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ height = 500
 win = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Projectile Motion with Pygame')
 
+playing = True
+time = 0
+power = 0
+angle = 0
+clock = pygame.time.Clock()
+
+while playing:
+    clock.tick(200)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+
 pygame.quit()
 quit()
